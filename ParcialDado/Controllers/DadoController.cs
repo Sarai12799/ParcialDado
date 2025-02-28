eb34e382719fea2621f830e72397f91741e6f281
ALUMNO PC9 <ALUMNO PC9@LABCOMP3PC9>
2025-02-28 10:41:06 -0600

    Agregar archivos de proyecto.

---
+++ ParcialDado/Controllers/DadoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParcialDado.Models;
+
+namespace ParcialDado.Controllers
+{
+    public class DadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calcular(Dado obdado)
+        {
+            Random rnd = new Random();
+            obdado.NumeroGenerado = rnd.Next(1, 40);
+            return View("Resultado", obdado);
+        }
+
+    }
+}
---
+++ ParcialDado/Models/Dado.cs
@@ -0,0 +1,11 @@
+﻿namespace ParcialDado.Models
+{
+    public class Dado
+    {
+        private static Random random = new Random();
+        public int NumeroGenerado { get; set; }
+        public double TiempoCaida { get; set; }
+        public double Altura { get; set; }
+        
+    }
+}
